1c2ace3ae929257f8efff1467a772555b16de5ae
jmschoenborn <jmschoenborn@gmx.de>
2024-06-18 13:28:57 +0200

    Create vuln-website.php

---
+++ vuln-website.php
@@ -0,0 +1,34 @@
+<?php
+// Configuration for the database connection
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "test_db";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Get the 'id' parameter from the URL
+$id = $_GET['id'];
+
+// Construct the SQL query
+$sql = "SELECT * FROM users WHERE id = $id";
+
+// Execute the query
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // Output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "id: " . $row["id"]. " - Name: " . $row["name"]. "<br>";
+    }
+} else {
+    echo "0 results";
+}
+$conn->close();
+?>
